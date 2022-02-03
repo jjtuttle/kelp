4a9c9bf78b5589f64cda32fb704a0f90cc3d00e5
James Tuttle <jaymz69@yahoo.com>
2022-02-03 15:13:40 -0800

    Added frontend and template for react and passing dispatch test action in console.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,10 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 
 
