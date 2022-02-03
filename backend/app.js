01394b07f02783fe4fda61374db0f3b99121977e
James Tuttle <jaymz69@yahoo.com>
2022-02-03 09:56:48 -0800

    Error handeling setup.

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
 const { environment } = require('./config');
-
+const { ValidationError } = require('sequelize');
 
 
 
@@ -46,6 +46,42 @@ app.use(
 );
 
 app.use(routes);
+
+// app.use( (err, req, res, next) => {
+//     console.log(err.stack);
+//     res.status(500).emd('Error');
+// });
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check to see if error is a sequelize error
+    if(err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error formatter 
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.log(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 
 
