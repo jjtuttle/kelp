2d2616b7aa9983643a42bce0c98ff823d12b4656
James Tuttle <jaymz69@yahoo.com>
2022-02-03 09:34:18 -0800

    Phase 1 done - API Routes.

---
+++ backend/routes/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+
+// test api router
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+
+
+
+
+
+
+module.exports = router;
+
