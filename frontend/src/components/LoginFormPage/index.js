21ec563629363179a1b1cd73f3b6985c30fd8926
James Tuttle <jaymz69@yahoo.com>
2022-02-04 13:50:00 -0800

    added images folder to src & user redirect after login done.

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,7 +3,11 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import './LoginForm.css';
+import profile from '../../images/profile-png.png';
+import email from '../../images/email-png.png';
+import pass from '../../images/pass-png.png';
+
+// import './LoginForm.css';
 
 const LoginFormPage = () => {
     const dispatch = useDispatch();
@@ -12,7 +16,7 @@ const LoginFormPage = () => {
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState([]);
 
-    if(sessionUser) return (
+    if (sessionUser) return (
         <Redirect to='/' />
     );
 
@@ -23,46 +27,57 @@ const LoginFormPage = () => {
         return dispatch(sessionActions.login({ credential, password }))
             .catch(async (res) => {
                 const data = await res.json();
-                if(data && data.errors) setErrors(data.errors);
+                if (data && data.errors) setErrors(data.errors);
             });
     }
- 
+
+
 
     return (
-    <div className='container'>
-        <form onSubmit={handleSubmit} className='login-form' >
-            <ul>
-                {errors.map((error, idx) => (
-                    <li key={idx}>{error}</li>
-                ))}
-            </ul>
-            <div>
-            <label>
-                Username or Email
-                <input 
-                    type='text'
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
-                />
-            </label>
+        <div className='container'>
+            <div className='sub-container'>
+                <div>
+                    <div className='img'>
+                        <div className='img-container'>
+                            {/* <img src={profile} alt='profile' className='profile' /> */}
+                        </div>
+                    </div>
+                    <form onSubmit={handleSubmit} className='login-form' >
+                        <ul>
+                            {errors.map((error, idx) => (
+                                <li key={idx}>{error}</li>
+                            ))}
+                        </ul>
+                        <div >
+                            <h2>Login</h2>
+                            <div>
+                                {/* <img src={email} alt='email' className='email' /> */}
+                                <input className='name'
+                                    type='text'
+                                    value={credential}
+                                    onChange={(e) => setCredential(e.target.value)}
+                                    required
+                                    placeholder='Username or Email'
+                                />
+                            </div>
+                            <div>
+                                {/* <img src={pass} alt='password' className='password' /> */}
+                                <input className='pass'
+                                    type='password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                    placeholder='Password'
+                                />
+                            </div>
+                        </div>
+                        <div className='login-btn'>
+                        <button type='submit' className='button'>Log In</button>
+                        </div>
+                    </form>
+                </div >
             </div>
-            <div>
-            <label>
-                Password
-                <input 
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            </div>
-            <div>
-            <button type='submit'> Log In</button>
-            </div>
-        </form> 
-    </div>
+        </div>
     );
 };
 
