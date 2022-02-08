9532136e3a19188da516cf26544165a31c7380d1
James Tuttle <jaymz69@yahoo.com>
2022-02-08 14:13:48 -0800

    need to switch to a brach so saving current then create a new branch.

--- backend/db/seeders/20220208183232-user-seed.js
+++ backend/db/seeders/20220208183232-user-seed.js
@@ -7,7 +7,7 @@ module.exports = {
       {email: 'demo@user.io', username:'demouser', hashedPassword:  bcrypt.hashSync('password')},
       {email: 'diver1@user.io', username:'diver1', hashedPassword:  bcrypt.hashSync('password')},
       {email: 'diver2@user.io', username:'diver2', hashedPassword:  bcrypt.hashSync('password')},
-  ], ); //{fields: ['email', 'username'], returning: true});
+  ], ); 
   },
 
   down: (queryInterface, Sequelize) => {
