355c1d8c9df6b9249ac4374329c15d84f44134f9
James Tuttle <jaymz69@yahoo.com>
2022-02-10 22:54:05 -0800

    added back the div's for render on the location details page.

--- frontend/src/components/Locations/Location.js
+++ frontend/src/components/Locations/Location.js
@@ -40,10 +40,36 @@ const Location = () => {
 
     return (
         <div className="main-container">
-            <h1>Dive Site Location - {loc?.title}</h1>
-            <p>{loc?.title}</p>
-            <p>{loc?.body}</p>
-            <p><span>{loc?.city}</span><span>{loc?.state}</span></p>
+            <div className="image">
+                <img alt='dive location' 
+                    // src={loc?.Images[0] ? loc?.Images[0].url : noImage }
+                />
+            </div>
+            <div className="username">
+                <span>Posted by: {user?.username} </span>
+            </div>
+            <div className="h1">
+                <h1>Dive Site Location - {loc?.title}</h1>
+            </div>
+            <div className="title">
+                <p><span>Title: {loc?.title}</span></p>
+            </div>
+            <div className="body">
+                <p>Body: {loc?.body}</p>
+            </div>
+            <div className="city">
+                <p><span>City: {loc?.city ? loc.city : 'No City Entered'}</span></p>
+            </div>
+            <div className="state">
+                <p><span>State: {loc?.state ? loc.state : 'No State Entered'}</span></p>
+            </div>
+            <div className="zip-code">
+                <p><span>Zip Code: {loc?.zipCode ? loc.zipCode : 'No Zip Code Entered'}</span></p>
+            </div>
+            <div className="btn-container">
+                <button className="delete-btn">DELETE</button><button className="cancel-btn">CANCEL</button>
+            </div>
+            
         </div>
 
     );
